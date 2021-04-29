6b4271512e929af3a85f0e075a725865845aa9d2
kawamii <kyuprgnbrz@gmail.com>
2021-04-29 21:51:10 +0900

    Initial Commit

---
+++ Word_listTests/Word_listTests.swift
@@ -0,0 +1,33 @@
+//
+//  Word_listTests.swift
+//  Word_listTests
+//
+//  Created by 川上知宏 on 2021/04/29.
+//
+
+import XCTest
+@testable import Word_list
+
+class Word_listTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
